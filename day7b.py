81615b9127d7b1dccaf3b8b52be998017a41d88b
Sardar Yumatov <yumatov.s@tkppensioen.nl>
2021-12-09 16:23:52 +0100

    Day 7 performance improvement

--- day7b.py
+++ day7b.py
@@ -1,4 +1,6 @@
-# brute forced
+def cost(n):
+    return n * (n + 1) / 2
+
 with open("day7.txt") as file:
     stand = [int(crab) for crab in file.readline().strip().split(',')]
-    print min(sum(sum(range(1, abs(crab - mid) + 1)) for crab in stand) for mid in range(min(stand), max(stand)+1))
+    print min(sum(cost(abs(crab - mid)) for crab in stand) for mid in range(min(stand), max(stand)+1))
